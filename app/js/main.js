fd9c7c4dda0624fa20c855959df20c8303abdd30
Daniil Zhdanov <yeezysmem@users.noreply.github.com>
2020-09-09 14:35:45 +0300

    !

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
+    $('.banner-slider').slick({
+      arrows:false,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+      });
 
+      $('.card').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false
+      });
 
 
   
